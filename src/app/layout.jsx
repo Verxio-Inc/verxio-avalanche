f7b2cb6f131eb4a67d46791759b3a25a33ddefb7
amdonatusprince <donatusprince@gmail.com>
2024-02-14 17:35:25 +0100

    rainbowkit integrate

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,11 +2,10 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import ReduxProvider from "../components/reduxProvider";
-import { NavProvider } from "../context/nav_context";
 import JunoProvider from "../components/junoProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import WagmiProviders from "../components/wagmiProviders";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,13 +22,13 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} font-inter`}>
-        {/* <NavProvider> */}
         <AppRouterCacheProvider>
+        <WagmiProviders>
           <JunoProvider>
             <ReduxProvider>{children}</ReduxProvider>
           </JunoProvider>
+          </WagmiProviders>
         </AppRouterCacheProvider>
-        {/* </NavProvider> */}
         <ToastContainer/>
       </body>
     </html>
