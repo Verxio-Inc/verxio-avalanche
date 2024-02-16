b16268c88204df86f3bd6abaaf4ac1d70d65d90b
amdonatusprince <donatusprince@gmail.com>
2024-02-16 10:33:36 +0100

    particle network fix

---
+++ src/app/dashboard/social-feed/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Social Feed</div>
+  )
+}
+
+export default page
--- src/lib/data/sideBarData.js
+++ src/lib/data/sideBarData.js
@@ -1,10 +1,11 @@
 import HandCoins from "../../assets/HandCoins.svg";
 import BriefcaseDollar from "../../assets/briefcase-dollar.svg";
 import BookOpen from "../../assets/BookOpen.svg";
-import Handshake from "../../assets/Handshake.svg"
-import Kanban from "../../assets/Kanban.svg"
-import Swap from "../../assets/Swap.svg"
-import Gear from "../../assets/Gear.svg"
+import Handshake from "../../assets/Handshake.svg";
+import Kanban from "../../assets/Kanban.svg";
+import Swap from "../../assets/Swap.svg";
+import Gear from "../../assets/Gear.svg";
+import Feed from "../../assets/feed.svg";
 
 export const NavigationItems = [
   {
@@ -16,6 +17,11 @@ export const NavigationItems = [
     tab: "Learn",
     icon: BookOpen,
     tabUrl: "/dashboard/learn",
+  },
+  {
+    tab: "Social Feed",
+    icon: Feed,
+    tabUrl: "/dashboard/social-feed",
   },
   // {
   //   tab: "DAO Proposals",
