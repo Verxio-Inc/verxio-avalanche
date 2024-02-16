78087234c6584583292bee780a8d017bd8ecb0a0
amdonatusprince <donatusprince@gmail.com>
2024-02-16 15:23:22 +0100

    rainbowkit fix

--- src/components/wagmiProviders.jsx
+++ src/components/wagmiProviders.jsx
@@ -1,69 +1,46 @@
 "use client";
-import { WagmiConfig, createConfig, configureChains } from "wagmi";
-import "@rainbow-me/rainbowkit/styles.css";
-import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
-import { publicProvider } from "wagmi/providers/public";
-import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import '@rainbow-me/rainbowkit/styles.css';
 import {
-  metaMaskWallet,
-  trustWallet,
-  injectedWallet,
-  rainbowWallet,
-  walletConnectWallet,
-  coinbaseWallet,
-} from "@rainbow-me/rainbowkit/wallets";
-import { PegasusTestnet } from "./Chain";
+  RainbowKitProvider,
+  getDefaultConfig
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+// import { PegasusTestnet } from "./Chain";
 
-const projectId = "274de4271228fdd69013c56274f0e688";
-const { chains, publicClient } = configureChains(
-  [PegasusTestnet],
-  [publicProvider()]
-);
-
-const connectors = connectorsForWallets([
-  {
-    groupName: "Recommended",
-    wallets: [
-      metaMaskWallet({ projectId, chains }),
-      trustWallet({ projectId, chains }),
-    ],
-  },
-  {
-    groupName: "Others",
-    wallets: [
-      coinbaseWallet({
-        chains,
-        appName: "Verxio | Decentralizing the future of work!",
-      }),
-      injectedWallet({ chains }),
-      rainbowWallet({ projectId, chains }),
-      walletConnectWallet({ projectId, chains }),
-    ],
-  },
-]);
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
+const config = getDefaultConfig({
+  appName: 'Verxio | Decentralizing the future of work!',
+  projectId: '274de4271228fdd69013c56274f0e688',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true
 });
 
+const queryClient = new QueryClient();
 const WagmiProviders = ({ children }) => {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider
-        theme={lightTheme({
-          accentColor: "#1570ef",
-          accentColorForeground: "white",
-          borderRadius: "small",
-          fontStack: "system",
-          overlayBlur: "small",
-        })}
-        chains={chains}
-      >
-        {children}
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+         <RainbowKitProvider
+          // theme={lightTheme({
+          //   accentColor: "#1570ef",
+          //   accentColorForeground: "white",
+          //   borderRadius: "small",
+          //   fontStack: "system",
+          //   overlayBlur: "small",
+          // })}
+          // chains={chains}
+        >
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
 
